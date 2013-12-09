44b31dc787a95a347a8f7e95881b8876b5ddb033
Hack Reactor Students <santiago.archila@gmail.com>
2013-12-09 15:48:40 -0800

    add router

--- web/basic-server.js
+++ web/basic-server.js
@@ -3,7 +3,19 @@ var handler = require("./request-handler");
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+
+var router = {
+  '/': handler.handleRequest
+};
+
+var server = http.createServer(function(req, res){
+  var path = req.url;
+  if (router[path]) {
+    router[path](req, res);
+  } else {
+    // 404
+  }
+});
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
