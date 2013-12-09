eea25df6bcd9f39ffc51ce5c3b4b674899b4ae98
Santiago <santiago.archila@gmail.com>
2013-12-09 14:56:22 -0800

    pass tests in workerSpec file

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,55 @@
 var path = require('path');
+var fs = require('fs');
+var httpHelpers = require('./http-helpers');
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
+var statusCode = 404;
+
+var text = "";
+
+fs.readFile(module.exports.datadir, function(err, fileContents){
+  text += fileContents;
+});
+
+var sendFinalResponse = function (req, res){
+  statusCode = 200;
+  res.writeHead(statusCode, httpHelpers.headers);
+  console.log(module.exports.datadir);
+  res.end(text);
+  console.log(exports.datadir);
+};
+
+var sendData = function (req, res){
+  sendFinalResponse(req, res);
+};
+
+var saveData = function(req, res){
+  var body = "";
+  req.on('data', function(chunk){
+    body += chunk;
+  });
+  req.on('end', function(){
+    text += body;
+    statusCode = 201;
+    sendFinalResponse(req, res);
+  });
+};
+
+var sendOptions = function(req, res){
+  sendFinalResponse(req, res);
+};
+
+var verbs = {
+  'GET': sendData,
+  'POST': saveData,
+  'OPTIONS': sendOptions
+};
 
 module.exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+  console.log("Received " + req.method + " request at URL " + req.url);
+  if (verbs[req.method]){
+    console.log("verb found");
+    verbs[req.method](req, res);
+  } else{
+    sendFinalResponse(req, res);
+  }
 };
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,5 +1,14 @@
-exports.readUrls = function(filePath, cb){
+var fs = require("fs");
+var path = require('path');
 
+exports.readUrls = function(filePath, cb){
+  fileText = "";
+  fs.readFile(filePath,function(err, fileContents){
+    fileText += fileContents;
+    console.log(filePath, fileText.split('\n'));
+    // console.log(JSON.parse(fileContents));
+    cb(fileText.split('\n'));
+  });
 };
 
 exports.downloadUrls = function(urls){
