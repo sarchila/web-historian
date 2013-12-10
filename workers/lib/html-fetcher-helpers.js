3b52d0c90a4c2298d08014117b06f0e36bfda58e
Hack Reactor Students <santiago.archila@gmail.com>
2013-12-10 15:53:51 -0800

    implement working html-fetcher-helpers

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,12 +1,20 @@
 var fs = require("fs");
 var path = require('path');
 var http = require('http-request');
-module.exports.datadir = path.join(__dirname, "../data/sites/"); // tests will need to override this.
+module.exports.datadir = path.join(__dirname, "../../data/sites/"); // tests will need to override this.
+
+var processUrl = function (err, res) {
+  if (err) {
+     console.log('Error fetching url\n', err);
+  }
+};
 
 exports.readUrls = function(filePath, cb){
   fileText = "";
   fs.readFile(filePath, function(err, fileContents){
     var urlArray = String(fileContents).split('\n');
+    urlArray.pop();
+    // console.log(urlArray);
     cb(urlArray);
   });
 };
@@ -15,14 +23,9 @@ exports.downloadUrls = function(urls){
   urls = urls || [];
 
   for (var i = 0; i < urls.length; i++) {
-    var path = module.exports.datadir + urls[i];
-    http.get(urls[i], path, function (err, res) {
-      if (err) {
-        console.log('Error fetching ' + urls[i]);
-      }
-    });
+    var newFilePath = module.exports.datadir + urls[i];
+    http.get({url:urls[i]}, newFilePath, processUrl);
   }
-
 
   return urls;
 };
