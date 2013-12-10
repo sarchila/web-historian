f8d87154ed85445f805187d5217e1356d4b024b6
Hack Reactor Students <santiago.archila@gmail.com>
2013-12-10 12:13:16 -0800

    refactor specs and implement readUrls functionality

--- spec/WorkerSpec.js
+++ spec/WorkerSpec.js
@@ -1,34 +1,68 @@
 var stubs = require("./helpers/stubs");
 var htmlFetcherHelpers = require("../workers/lib/html-fetcher-helpers");
+htmlFetcherHelpers.datadir = __dirname + "/testdata/sites/";
 var fs = require("fs");
 var path = require('path');
+var rmdir = require('rimraf');
+
+function async(cb){
+  waits(1000);
+  runs(cb);
+}
+
+beforeEach(function () {
+  fs.mkdirSync(htmlFetcherHelpers.datadir);
+});
+
+afterEach(function () {
+  rmdir.sync(htmlFetcherHelpers.datadir, function(err){
+    console.log("error removing dir: ", err);
+  });
+});
 
 describe("html fetcher helpers", function(){
 
-  it("should have a 'readUrls' function", function(){
-    var urlArray = ["example1.com", "example2.com"];
+  describe("readUrls", function(){
+    it("should have a 'readUrls' function", function(){
+      var urlArray = ["example1.com", "example2.com"];
 
-    var filePath = path.join(__dirname, "/testdata/sites.txt");
+      var filePath = path.join(__dirname, "/testdata/sites.txt");
 
-    fs.writeFileSync(filePath, urlArray.join("\n"));
+      fs.writeFileSync(filePath, urlArray.join("\n"));
 
-    var resultArray;
+      var resultArray;
 
-    runs(function(){
-      htmlFetcherHelpers.readUrls(filePath, function(urls){
-        resultArray = urls;
+      runs(function(){
+        htmlFetcherHelpers.readUrls(filePath, function(urls){
+          resultArray = urls;
+        });
+      });
+
+      waits(1000);
+
+      runs(function() {
+        expect(resultArray).toEqual(urlArray);
+      });
+    });
+  });
+
+  describe("downloadUrls", function(){
+
+    it("should have a 'downloadUrls' function", function(){
+      var result = htmlFetcherHelpers.downloadUrls();
+      expect(result).toBeTruthy();
+    });
+
+    it("should save input URLs to sites folder", function () {
+      htmlFetcherHelpers.downloadUrls(['127.0.0.1:8080']);
+      waits(1000);
+      runs(function(){
+        var files = fs.readdirSync(htmlFetcherHelpers.datadir);
+        expect(files).toEqual(['127.0.0.1:8080']);
       });
     });
 
-    waits(1000);
-
-    runs(function() {
-      expect(resultArray).toEqual(urlArray);
-    });
   });
 
-  xit("should have a 'downloadUrls' function", function(){
-    var result = htmlFetcherHelpers.downloadUrls();
-    expect(result).toBeTruthy();
-  });
+
 });
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
 var path = require('path');
+var http = require('http-request');
+module.exports.datadir = path.join(__dirname, "../data/sites/"); // tests will need to override this.
 
 exports.readUrls = function(filePath, cb){
   fileText = "";
@@ -10,5 +12,17 @@ exports.readUrls = function(filePath, cb){
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
+  urls = urls || [];
+
+  for (var i = 0; i < urls.length; i++) {
+    var path = module.exports.datadir + urls[i];
+    http.get(urls[i], path, function (err, res) {
+      if (err) {
+        console.log('Error fetching ' + urls[i]);
+      }
+    });
+  }
+
+
+  return urls;
 };
