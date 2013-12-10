e33cd1e835a6e95aac747c9ad9cf8d9560370b8c
Hack Reactor Students <santiago.archila@gmail.com>
2013-12-10 15:47:27 -0800

    implement html fetcher file that runs 1x/min

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,7 @@
 // eventually, you'll have some code here that uses the tested helpers 
 // to actually download the urls you want to download.
+var helpers = require('./lib/html-fetcher-helpers');
+var path = require('path');
+var sitesPath = path.join(__dirname, "../data/sites.txt");
+
+helpers.readUrls(sitesPath, helpers.downloadUrls);
