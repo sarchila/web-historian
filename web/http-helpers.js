2f57ffff83e33bdf8aaae985874768982b948349
Hack Reactor Students <santiago.archila@gmail.com>
2013-12-10 21:40:02 -0800

    removed unused methods from headers

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-methods": "GET, POST, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10, // Seconds.
   'Content-Type': "text/html"
